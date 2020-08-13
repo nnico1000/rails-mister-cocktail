42a34afba7683bcd0bd679b22e74b34b2d17aaf4
nicolas chevalier <nicolas_chevalier08@hotmail.fr>
2020-08-13 17:01:41 +0200

    cocktails v2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,13 @@
 class DosesController < ApplicationController
-
   def new
+    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to dose_path(@dose)
     else
@@ -23,6 +25,6 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:name)
+    params.require(:dose).permit(:description)
   end
 end
